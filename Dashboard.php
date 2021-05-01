7793da150700487e63a174ceafa2bcce2ff708d8
Ramalingasamy M K <69101768+Ramalingasamy012@users.noreply.github.com>
2021-05-01 07:46:21 +0530

    Add files via upload

---
+++ Dashboard.php
@@ -0,0 +1,30 @@
+<html>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<body>
+<div class="jumbotron">
+ <h1 class="display-4">GROOTAN USERS</h1>
+<ul class="list-group pmd-list pmd-card-list">
+<?php
+$string = "https://grootanusers.free.beeceptor.com/users";
+$data = json_decode(file_get_contents($string),true);
+ foreach($data as $val)
+ {
+	 echo "<li class='list-group-item d-flex align-items-center'>
+        <div class='media-body'>
+            ".$val['name']."
+        </div>
+		<form action='users.php' method='POST'>
+		<input type='hidden' name='id' value='".$val['userid']."'>
+        <button type='submit' class='btn btn-default'><i class='fa fa-eye'></i></button>
+		</form>
+    </li>";
+ }
+?>
+</ul>
+</div>
+</body>
+</html>
---
+++ users.php
@@ -0,0 +1,100 @@
+<?php
+$id=$_POST['id'];
+$string = "https://grootanusers.free.beeceptor.com/userid=".$id;
+$data = json_decode(file_get_contents($string),true);
+?>
+<html>
+<head>
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+</head>
+<style>
+
+
+.glyphicon {  margin-bottom: 10px;margin-right: 10px;}
+
+small {
+display: block;
+line-height: 1.428571429;
+color: #999;
+}
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.header {
+  padding: 5px 10px;
+  background: #555;
+  color: #f1f1f1;
+  text-align:center;
+}
+.container
+{
+	padding-top:80px;
+	padding-bottom:80px;
+	padding-left:250px;
+	
+}
+
+</style>
+<body>
+<div class="header" id="myHeader">
+  <h2>User Details</h2>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12 col-sm-6 col-md-6">
+            <div class="well well-sm">
+                <div class="row">
+                    <div class="col-sm-6 col-md-4">
+                        <img src="https://i.pinimg.com/originals/7c/c7/a6/7cc7a630624d20f7797cb4c8e93c09c1.png" alt="" class="img-rounded img-responsive" />
+                    </div>
+                    <div class="col-sm-6 col-md-8">
+                        <h4>
+                            <?php echo $data['name']; ?></h4>
+                        
+                        <p>
+						
+                            <b> User ID   :</b> <?php echo $data['userid']; ?>
+                      
+							<hr>
+							<b> DOB       :</b> <?php echo $data['dob']; ?>
+                            <hr>
+				
+							<b> Age       :</b> <?php echo $data['age']; ?>
+                            <hr>
+							<b> Address   :</b> <?php echo $data['more']['address_line1'].','.$data['more']['address_line2'].','.$data['more']['address_line3']; ?>
+                            <hr>
+							<b> Phone number: </b> <?php echo $data['more']['phone']; ?>
+                           <hr>
+						</p>
+						<?php
+						if($id==1)
+						{
+						echo "<form action='users.php' method='POST'>
+					   <input type='hidden' name='id' value=".($id+1)."><button type='submit' class='btn btn-primary'>Next</i></button>
+					   </form>";
+						}
+						else
+						{
+							echo "<form action='users.php' method='POST'>
+					   <input type='hidden' name='id' value=".($id-1)."><button type='submit' class='btn btn-primary'>Previous</i></button>
+					   </form>";
+					   echo "<form action='users.php' method='POST'>
+					   <input type='hidden' name='id' value=".($id+1)."><button type='submit' class='btn btn-primary'>Next     </i></button>
+					   </form>";
+						}
+						?>
+                       
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
